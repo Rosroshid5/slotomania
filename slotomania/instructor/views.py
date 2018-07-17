88945d19e8975ba7ffb789dbdb0a7cfc0cb64f33
Conan Li <conanlics@gmail.com>
2018-07-17 14:37:58 -0400

    response

--- slotomania/instructor/views.py
+++ slotomania/instructor/views.py
@@ -1,7 +1,7 @@
 from typing import Type, Dict
 from django.db import transaction
 from django.http import HttpResponse
-from rest_framework.response import Response
+from rest_framework import response
 from rest_framework.generics import GenericAPIView
 
 from slotomania.contrib.request_resolver import RequestResolver
@@ -12,20 +12,20 @@ class InstructorView(GenericAPIView):
     routes: Dict[str, Type[RequestResolver]]
 
     def get(self, request, endpoint: str = None):
-        return Response({})
+        return response.Response({})
 
     @transaction.atomic
-    def post(self, request, endpoint: str) -> Response:
+    def post(self, request, endpoint: str) -> response.Response:
         """If mustate_state returns HttpResponse, return it."""
         resolver = self.routes[endpoint](request=request, data=request.data)
         response = resolver.resolve()
         if isinstance(response, HttpResponse):
             return response
         elif hasattr(response, "sloto_to_dict"):
-            return Response(response.sloto_to_dict())
+            return response.Response(response.sloto_to_dict())
         elif isinstance(response, dict):
-            return Response(response)
+            return response.Response(response)
         elif hasattr(response, 'serialize'):
-            return Response(response.serialize())
+            return response.Response(response.serialize())
         else:
             raise AssertionError('Unknow type: {}'.format(type(response)))
