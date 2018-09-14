f194328cad2eee58085285d564ecd696eee876c6
Conan Li <conanlics@gmail.com>
2018-09-14 15:44:12 -0400

    bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="slotomania",
-    version="0.1.4",
+    version="0.1.5",
     description="Slotomania",
     long_description=long_description,
     scripts=["bin/fix_sloto_stubs.py"],
